028640809cd24cfaf2550d9a3ab956c5b79d4195
Jonathan Neal <jonathantneal@hotmail.com>
2022-02-02 22:28:05 -0500

    0.1.0

--- demo/astro.config.js
+++ demo/astro.config.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import { myPlugin } from '@example/my-plugin'
 
 /** @type {import('astro').AstroUserConfig} */
--- packages/my-plugin/index.js
+++ packages/my-plugin/index.js
@@ -1,4 +1,6 @@
-/** @type {import('vite').Plugin} */
+// @ts-check
+
+/** @type {{ (options?: Record<string, any>): import('vite').Plugin }} */
 export const myPlugin = (options) => {
 	void options
 
@@ -6,19 +8,31 @@ export const myPlugin = (options) => {
 		name: '@example/my-plugin',
 		// optional hooks
 
-		// https://vitejs.dev/guide/api-plugin.html#configresolved
-		configResolved(resolvedConfig) {},
+		// // https://vitejs.dev/guide/api-plugin.html#configresolved
+		// configResolved(resolvedConfig) {},
 
-		// https://rollupjs.org/guide/en/#resolveid
-		resolveId(importee, importer, options, isSSR) {},
+		// // https://rollupjs.org/guide/en/#resolveid
+		// resolveId(importee, importer, options, isSSR) {
+		// 	return undefined
+		// },
 
-		// https://vitejs.dev/guide/api-plugin.html#configureserver
-		configureServer(server) {},
+		// // https://vitejs.dev/guide/api-plugin.html#configureserver
+		// configureServer(server) {},
 
-		// https://rollupjs.org/guide/en/#load
-		load(importee, isSSR) {},
+		// // https://rollupjs.org/guide/en/#load
+		// load(importee, isSSR) {
+		// 	return undefined
+		// },
 
-		// https://rollupjs.org/guide/en/#transform
-		transform(code, importee, isSSR) {},
+		// // https://rollupjs.org/guide/en/#transform
+		// transform(code, importee, isSSR) {
+		// 	return undefined
+		// },
+
+		// // https://rollupjs.org/guide/en/#buildstart
+		// buildStart() {},
+
+		// // https://rollupjs.org/guide/en/#buildend
+		// buildEnd() {}
 	}
 }
